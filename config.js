8655c0396d217eabaf732633c99aa16ced383050
nguyenziet <117888610+nguyenziet@users.noreply.github.com>
2022-11-23 17:35:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em iuuu<3';
+const giftUrl = 'https://www.youtube.com/watch?v=1ALM5ZfAGyQ';
+const eventName = 'Chúc mừng sinh nhật';
+const titleCard = 'Tran Chuu';
+const contentCard = 'Chúc em người yêu sinh nhật zui zẻ. Luôn may mắn, xinh đẹp và bớt nóng tính đi nhé:)) <br> Tính nóng như kem đấy';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'a3.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
